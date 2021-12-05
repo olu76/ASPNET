26c06c8f6b2aa95000506d0157ead7caa7386b53
olumide azeez afolabi <midas44000@yahoo.com>
2021-12-05 04:17:54 -0600

    initial commit

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+        private IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * From products");
+        }
+
+
+
+
+            
+    }
+}
