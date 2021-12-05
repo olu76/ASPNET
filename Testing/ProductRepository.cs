8c15f8a4cdef37fb5c8774c5c92ab8e0c5da74b7
olumide azeez afolabi <midas44000@yahoo.com>
2021-12-05 05:26:19 -0600

    Finished View Product (2) & Update Product (3)

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -7,9 +7,8 @@ namespace Testing
 {
     public class ProductRepository : IProductRepository
     {
-        private readonly IDbConnection _connection;
-        private IDbConnection _conn;
-
+        private readonly IDbConnection _conn;
+        
         public ProductRepository(IDbConnection conn)
         {
             _conn = conn;
@@ -20,9 +19,19 @@ namespace Testing
             return _conn.Query<Product>("Select * From products");
         }
 
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+             new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
 
 
-
-            
     }
 }
+
