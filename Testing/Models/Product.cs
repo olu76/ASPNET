4bac728c689127d546f9c86abd259cd3517f48fc
olumide azeez afolabi <midas44000@yahoo.com>
2021-12-05 06:07:42 -0600

    Create Product (4) & Delete Product (5)

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.Models
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -11,5 +13,7 @@
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
     }
 }
